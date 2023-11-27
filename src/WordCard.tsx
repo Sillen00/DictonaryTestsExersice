340386dee711b4f744dc7fd03ddf422ad84269e8
Simon <sillen.00@hotmail.com>
2023-11-27 15:56:55 +0100

    show data the right way.

--- src/WordCard.tsx
+++ src/WordCard.tsx
@@ -5,47 +5,132 @@ function WordCard({ searchResultWord }) {
     return (
         <div className="container">
             <div className="wordCard">
-                {searchResultWord.map((wordObj) => (
-                    <div key={wordObj.word}>
-                        <h1>{wordObj.word}</h1>
+                {searchResultWord.length > 0 && (
+                    <div key={searchResultWord[0].word}>
+                        <h1>{searchResultWord[0].word}</h1>
 
                         <br />
                         <hr />
                         <br />
 
-                        {/* Meanings */}
-                        {wordObj.meanings.map((meaning, index) => (
-                            <div key={index}>
-                                <h5>{meaning.partOfSpeech}</h5>
-                                <p>
-                                    Definition:{" "}
-                                    {meaning.definitions[0].definition}
-                                </p>
-                            </div>
-                        ))}
+                        {/* Meanings*/}
+                        {searchResultWord[0].meanings.length > 0 && (
+                            <>
+                                {/* Part of speech & definition */}
+                                <div>
+                                    <h5>
+                                        {
+                                            searchResultWord[0].meanings[0]
+                                                .partOfSpeech
+                                        }
+                                    </h5>
+                                    <p>
+                                        Definition:{" "}
+                                        {
+                                            searchResultWord[0].meanings[0]
+                                                .definitions[0].definition
+                                        }
+                                    </p>
+                                    <br />
+                                    <hr />
+                                    <br />
+                                </div>
 
-                        <br />
-                        <hr />
-                        <br />
+                                {/* Word using example */}
+                                <div>
+                                    {searchResultWord.length > 0 &&
+                                        searchResultWord[0].meanings[0]
+                                            .definitions[0].example && (
+                                            <>
+                                                <p>
+                                                    Example:{" "}
+                                                    {
+                                                        searchResultWord[0]
+                                                            .meanings[0]
+                                                            .definitions[0]
+                                                            .example
+                                                    }
+                                                </p>
+                                                <br />
+                                                <hr />
+                                                <br />
+                                            </>
+                                        )}
+                                </div>
+
+                                {/* Synonymer */}
+                                <div>
+                                    {searchResultWord.length > 0 &&
+                                        searchResultWord[0].meanings[0].synonyms
+                                            .length > 0 && (
+                                            <>
+                                                <p>
+                                                    Synonyms:{" "}
+                                                    {searchResultWord[0].meanings[0].synonyms.map(
+                                                        (synonym, index) => (
+                                                            <span key={index}>
+                                                                {synonym}{" "}
+                                                            </span>
+                                                        )
+                                                    )}
+                                                </p>
+                                                <br />
+                                                <hr />
+                                                <br />
+                                            </>
+                                        )}
+                                </div>
+
+                                {/* Motsats ord */}
+                                <div>
+                                    {searchResultWord.length > 0 &&
+                                        searchResultWord[0].meanings[0].antonyms
+                                            .length > 0 && (
+                                            <>
+                                                <p>
+                                                    Antonyms:{" "}
+                                                    {searchResultWord[0].meanings[0].synonyms.map(
+                                                        (synonym, index) => (
+                                                            <span key={index}>
+                                                                {synonym}{" "}
+                                                            </span>
+                                                        )
+                                                    )}
+                                                </p>
+                                                <br />
+                                                <hr />
+                                                <br />
+                                            </>
+                                        )}
+                                </div>
+                            </>
+                        )}
 
                         {/* Phonetics */}
-                        {wordObj.phonetics.map((phonetic, index) => (
-                            <div key={index}>
-                                <p>Phonetic: {phonetic.text}</p>
+                        {searchResultWord.length > 0 &&
+                            searchResultWord[0].phonetics.length > 0 && (
+                                <div>
+                                    <p>
+                                        Phonetic:{" "}
+                                        {searchResultWord[0].phonetics[0].text}
+                                    </p>
 
-                                {/* Audio */}
-                                <audio controls>
-                                    <source
-                                        src={phonetic.audio}
-                                        type="audio/mpeg"
-                                    />
-                                    Your browser does not support the audio
-                                    element.
-                                </audio>
-                            </div>
-                        ))}
+                                    {/* Audio */}
+                                    <audio controls>
+                                        <source
+                                            src={
+                                                searchResultWord[0].phonetics[0]
+                                                    .audio
+                                            }
+                                            type="audio/mpeg"
+                                        />
+                                        Your browser does not support the audio
+                                        element.
+                                    </audio>
+                                </div>
+                            )}
                     </div>
-                ))}
+                )}
             </div>
         </div>
     );
